721406dcb2fee58ff73d7edda71dbc6f0f0bd777
Даниил Истомин <danyistomin0208@yandex.ru>
2018-12-18 12:06:48 +0300

    изменение файла

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -14,49 +14,59 @@ bool check(const char c, const char *sp);
 void strcpy(char* s1, const char *s2, int size);
 
 int main() {
-  int i, j;
-  int size;
-  int count = 0;
-  char *word;
-  char buf[MAS_SIZE];
-  ifstream in("add/in.txt");
-  if(!in.is_open()) {
-    cout << "Error: file is not opened" << endl;
-    in.close();
-    return 0;
-  }
-  for(i = 0; (buf[i] = in.get()) != EOF; ++i);
-  buf[i--] = '\0';
-  size = i;
-  cout << "Original text: " << buf << endl;
-  for(i = 0; i < size; ++i) {
-    if(!check(buf[i], sep)) {
-      if(check(buf[i], vow)) {
-        for(j = i + 1; j < size; ++j)
-          if(check(buf[j], sep)) break;
-        if(check(buf[j - 1], vow)) {
-          word = new char[j - i + 1];
-          strcpy(word, buf + i, j - i);
-          word[j - i] = '\0';
-          cout << "Word " << count++ + 1 << ": " << word << endl;
-          delete[] word;
-          i = j;
-        }
-      }
-    }
-  }
-  if(!count)
-    cout << "Choose another words, and please try again";
-  in.close();
-  return 0;
+	int i, j;
+	int size;
+	int count = 0;
+	char *word;
+	char buf[MAS_SIZE];
+	ifstream in("in.txt");
+	if (!in.is_open()) {
+		cout << "Error: file is not opened" << endl;
+		in.close();
+		return 0;
+	}
+	for (i = 0; (buf[i] = in.get()) != EOF; ++i); in.close();
+	buf[i] = '\0';
+	size = i;
+	cout << "Original text: " << buf << endl;
+	for (i = 0; i < size; ++i) {
+		
+		if (!check(buf[i], sep)) {
+			if (check(buf[i], vow)) {
+				for (j = i + 1; j < size; ++j)
+					if (check(buf[j], sep)) break;
+				if (check(buf[j - 1], vow)) {
+					word = new char[j - i + 1];
+					strcpy(word, buf + i, j - i);
+					word[j - i] = '\0';
+					cout << "Word " << ++count << ": " << word << endl;
+					delete[] word;
+					i = j;
+				}
+			}
+			else {
+				for (; i < size; ++i)
+					if (check(buf[i], sep)) break;
+			}
+		}
+		
+	}
+	
+	if (!count)
+		cout << "Oops, another time :)";
+	in.close();
+
+	cin >> i;
+	return 0;
 }
 
 bool check(const char c, const char *sp) {
-  for(int i = 0; sp[i] != '\0'; ++i)
-    if(sp[i] == c) return true;
-  return false;
+	if (c == '\0') return false;
+	for (int i = 0; sp[i] != '\0'; ++i)
+		if (sp[i] == c) return true;
+	return false;
 }
 void strcpy(char* s1, const char *s2, int size) {
-  for(int i = 0; i < size; ++i)
-    s1[i] = s2[i];
+	for (int i = 0; i < size; ++i)
+		s1[i] = s2[i];
 }
