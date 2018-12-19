7e8aaf8eec3bebc018ccde6e66aa3ccc1a38dae2
Даниил Истомин <danyistomin0208@yandex.ru>
2018-12-19 21:16:00 +0300

    Блоки исключений

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,97 +1,90 @@
-	#include <iostream>
-	#include <fstream>
+#include <iostream>
+#include <fstream>
 
-	#define MAS_SIZE 1024
-	const char sep[] = ". ,!?:;_\n";
-	const char vow[] = "aeiouy";
+#define MAS_SIZE 1024
+const char sep[] = ". ,!?:;_\n";
+const char vow[] = "aeiouyAEIOUY";
 
-	using std::cin;
-	using std::cout;
-	using std::endl;
-	using std::ifstream;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
 
-	bool check(const char c, const char *sp);
-	void strcpy(char* s1, const char *s2, int size);
+bool check(const char c, const char *sp);
+void strcpy(char* s1, const char *s2, int size);
 
-	int main() {
-		int i, j;
-		int size;
-		int count = 0;
-		char *word;
-		char buf[MAS_SIZE];
-		ifstream in("in.txt");
-		if (!in.is_open()) {
-			cout << "Error: file is not opened" << endl;
-			in.close();
-			return 0;
-		}
-		for (i = 0; (buf[i] = in.get()) == EOF; ++i) {
-		try {
-			for (i = 0; (buf[i] = in.get()) != EOF && i < MAS_SIZE; ++i);
-			if (buf[i] == EOF) {
-				buf[i] = '\0';
-				size = i;
-				in.close();
-			}
-			else {
-				throw "File > MAS_SIZE";
-			}
-		}
-		catch (const char* str) {
-			cout << "Error, " << str << endl;
-			in.close();
-			return 0;
-			}
-		}
-		
-		for (i = 0; (buf[i] = in.get()) != EOF; ++i); 
-
-	
-
-		buf[i] = '\0';
-		size = i;
-		cout << "Original text: " << buf << endl;
-
-
-		for (i = 0; i < size; ++i) {
-		
-			if (!check(buf[i], sep)) {
-				if (check(buf[i], vow)) {
-					for (j = i + 1; j < size; ++j)
-						if (check(buf[j], sep)) break;
-					if (check(buf[j - 1], vow)) {
-						word = new char[j - i + 1];
-						strcpy(word, buf + i, j - i);
-						word[j - i] = '\0';
-						cout << "Word " << ++count << ": " << word << endl;
-						delete[] word;
-						i = j;
-					}
-				}
-				else {
-					for (; i < size; ++i)
-						if (check(buf[i], sep)) break;
-				}
-			}
-		
-		}
-	
-		if (!count)
-			cout << "Choose another words";
+int main() {
+	int i, j;
+	int size;
+	int count = 0;
+	char *word;
+	char buf[MAS_SIZE];
+	ifstream in("in.txt");
+	if (!in.is_open()) {
+		cout << "Error: file is not opened" << endl;
 		in.close();
-
-		cin >> i;
 		return 0;
 	}
 
-	bool check(const char c, const char *sp) {
-		if (c == '\0') return false;
-		for (int i = 0; sp[i] != '\0'; ++i)
-			if (sp[i] == c) return true;
-		return false;
+	try {
+		for (i = 0; (buf[i] = in.get()) != EOF && i < MAS_SIZE; ++i);
+		if (buf[i] == EOF) {
+			buf[i] = '\0';
+			size = i;
+			in.close();
+		}
+		else {
+			throw "File > MAS_SIZE";
+		}
 	}
-	void strcpy(char* s1, const char *s2, int size) {
-		for (int i = 0; i < size; ++i)
-			s1[i] = s2[i];
+	catch (const char* str) {
+		cout << "Error, " << str << endl;
+		in.close();
+		return 0;
 	}
 
+
+	cout << "Original text: " << buf << endl;
+
+
+	for (i = 0; i < size; ++i) {
+
+		if (!check(buf[i], sep)) {
+			if (check(buf[i], vow)) {
+				for (j = i + 1; j < size; ++j)
+					if (check(buf[j], sep)) break;
+				if (check(buf[j - 1], vow)) {
+					word = new char[j - i + 1];
+					strcpy(word, buf + i, j - i);
+					word[j - i] = '\0';
+					cout << "Word " << ++count << ": " << word << endl;
+					delete[] word;
+					i = j;
+				}
+			}
+			else {
+				for (; i < size; ++i)
+					if (check(buf[i], sep)) break;
+			}
+		}
+
+	}
+
+	if (!count)
+		cout << "Choose another words";
+	in.close();
+
+	cin >> i;
+	return 0;
+}
+
+bool check(const char c, const char *sp) {
+	if (c == '\0') return false;
+	for (int i = 0; sp[i] != '\0'; ++i)
+		if (sp[i] == c) return true;
+	return false;
+}
+void strcpy(char* s1, const char *s2, int size) {
+	for (int i = 0; i < size; ++i)
+		s1[i] = s2[i];
+}
